b5cf61e2dc30d5a5a550b5c7f5e90cb0073c8ab6
Fady Faheem <fadyfaheem@hotmail.com>
2023-07-13 06:57:33 -0500

    Update FirebaseAuthService.js

--- Firebase/Services/FirebaseAuthService.js
+++ Firebase/Services/FirebaseAuthService.js
@@ -15,15 +15,37 @@ const registerUser = (email, password) => {
 };
 
 const loginUser = (email, password) => {
-  return signInWithEmailAndPassword(auth, email, password);
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      return "Logged in";
+    })
+    .catch((error) => {
+      return error.code;
+    });
 };
 
 const logOutUser = () => {
   return signOut(auth);
 };
 
-const sendResetEmail = (email) => {
-  return sendPasswordResetEmail(auth, email);
+const sendResetEmail = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    return "Email sent!";
+  } catch (error) {
+    switch (error.code) {
+      case "auth/missing-email":
+        return "Email is required. Please try again.";
+      case "auth/invalid-email":
+        return "Invalid email. Please try again.";
+      case "auth/user-not-found":
+        return "Email is not registered. Please try again.";
+      case "auth/too-many-requests":
+        return "Too many requests. Try again later.";
+      default:
+        return error.code;
+    }
+  }
 };
 
 const subscribeToAuthChanges = (handleAuthChange) => {
